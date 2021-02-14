524329795e2aff668807b72995c91347867361aa
Cenk Gokturk <cenkgokturk06@gmail.com>
2021-02-15 00:26:41 +0300

    New project for sessions has been added

---
+++ User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETAOP_Session
+{
+    public class User
+    {
+        public String[] UserInfo = new String[2];
+        //0 - username
+        //1 - usermail
+
+        public static readonly User CurrentUser = new User();
+
+        public User(String[] UserInfo)
+        {
+            this.UserInfo = UserInfo;
+        }
+
+        public User(String username, String mail)
+        {
+            UserInfo[0] = username;
+            UserInfo[1] = mail;
+        }
+
+        public User(){}
+
+        public void setUsername( String Username) { this.UserInfo[0] = Username; }
+        public void setMail(String mail) { this.UserInfo[1] = mail; }
+
+        public String getUsername() { return UserInfo[0]; }
+        public String getUsermail() { return UserInfo[1]; }
+    }
+}
---
+++ UserSession.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETAOP_Session
+{
+    public class UserSession
+    {
+        private IConfiguration _configuration;
+        public UserSession(IConfiguration Configuration) { _configuration = Configuration; }
+
+        public UserSession() { }
+
+        public int SetUser(User CurrentUser) {
+
+            int id = -1;
+
+            String connection = _configuration.GetConnectionString("localDatabase");
+            using (SqlConnection sqlconn = new SqlConnection(connection))
+            {
+
+                String username = CurrentUser.getUsername();
+                String usermail = CurrentUser.getUsermail();
+
+                string sqlquery = "insert into UserSessions(Username, Usermail) values ('" + username + "', '" + usermail + "' ) " +
+                    "select ID from UserSessions where usermail '" + usermail + "'";
+                using (SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn))
+                {
+                    sqlconn.Open();
+                    SqlDataReader reader = sqlcomm.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            id = reader.GetInt32(0);
+                            Console.WriteLine("id in setUser method in  UserSession.cs: " + id);
+                        }
+                    }
+                    reader.Close();
+
+                }
+            }
+
+            return id;
+        }
+
+        public User GetUser(int id)
+        {
+            String username = "";
+            String usermail = "";
+
+            String connection = _configuration.GetConnectionString("localDatabase");
+            using (SqlConnection sqlconn = new SqlConnection(connection))
+            {
+
+                string sqlquery = "select Username, Usermail from UserSessions where ID = '" + id + "'";
+                using (SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn))
+                {
+                    sqlconn.Open();
+                    SqlDataReader reader = sqlcomm.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            username = reader.GetString(0);
+                            usermail = reader.GetString(1);
+
+                            Console.WriteLine("Username && Usermail in getUser method in  UserSession.cs");
+                            Console.WriteLine(username + "," + usermail);
+                        }
+                    }
+                    reader.Close();
+
+                }
+            }
+
+            String[] Info = {username, usermail };
+
+
+            return new User(Info);
+        }
+
+    }
+}
